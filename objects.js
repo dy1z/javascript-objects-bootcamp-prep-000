a2f00c6bbbf1fe725019e49653db54dd2b344594
Dylan <dylan@jumponweb.com.au>
2018-06-22 09:07:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 var playlist = { JohnLegend: "All of me" };
 
-function updatePlaylist ({ }, artist, song) {
+function updatePlaylist ({}, artist, song) {
   return Object.assign({}, 'neo','Genta')
 }
 function removeFromPlaylist (playlist, artistName) {
   delete playlist.artistName;
+  return playlist
 }
